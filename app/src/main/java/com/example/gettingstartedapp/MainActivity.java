6fb1a5d81f03b99028440133e25e3e28255e1e77
EgorK0rshun <egork@digitalchemy.us>
2023-10-24 18:35:32 +0300

    Add Implicit intent

--- app/src/main/java/com/example/gettingstartedapp/MainActivity.java
+++ app/src/main/java/com/example/gettingstartedapp/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         cardView.setOnClickListener(view -> {
             Intent cardActivityIntent = new Intent(MainActivity.this, CardActivity.class);
             startActivity(cardActivityIntent);
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchEmailIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailIntent.setData(Uri.parse("mailto:egork@digitalchemy.us"));
+                startActivity(launchEmailIntent);
+            }
         });
     }
 }
